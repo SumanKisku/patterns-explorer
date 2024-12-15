2da7ceaa2f4ddef2bc2af626ee6e6ba127455bae
Suman Kisku <sumankisku1@gmail.com>
2024-12-16 02:32:19 +0530

    Added SSG

--- app/pattern/[name]/page.tsx
+++ app/pattern/[name]/page.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import { useParams } from "next/navigation";
+import Image from "next/image";
 import { patterns } from "@/data/patterns";
 
 interface Pattern {
@@ -9,12 +7,8 @@ interface Pattern {
   imageUrl: string;
 }
 
-interface Props {
-  pattern: Pattern;
-}
-
-export default function PatternDetail() {
-  const { name } = useParams();
+export default async function PatternDetail({ params }: { params: Promise<{ name: string }> }) {
+  const name = (await params).name;
 
   // Find the pattern based on the name
   const pattern = patterns.find((p) => p.id.toString() === name);
@@ -27,12 +21,17 @@ export default function PatternDetail() {
   return (
     <div className="max-w-2xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">{pattern.name}</h1>
-      <img src={pattern.imageUrl} alt={pattern.name} className="w-full h-auto mb-4" />
+      <Image src={pattern.imageUrl} alt={pattern.name} className="w-full h-auto mb-4" />
       <p className="text-lg text-gray-700">
         {/* You can add a description or any other details about the pattern here */}
         This pattern represents a {pattern.name} in technical analysis.
       </p>
     </div>
   );
+};
+
+export async function generateStaticParams() {
+  return patterns.map((p: Pattern) => ({
+    id: p.id.toString(),
+  }))
 }
-;
